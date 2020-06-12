0a4763ebc380f23a062847489152716e7a336570
zxzx123 <370988097@qq.com>
2020-06-12 22:08:08 +0800

    no message

--- application/fladmin/controller/Login.php
+++ application/fladmin/controller/Login.php
@@ -33,7 +33,7 @@ class Login extends Controller
 
         $res = logic('Admin')->login($_POST);
         if ($res['code'] === ReturnData::SUCCESS) {
-            session('admin_info', $res['data']);
+            session('admin_info', $res['data']); $this->notice();
             $this->success('登录成功', url('fladmin/index/index'), '', 1);
         }
 
